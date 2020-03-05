8977f707c609eb4fbbf2080bae105608a1daf918
Natasa_C <57111995+Natasa-C@users.noreply.github.com>
2020-03-05 10:12:52 +0200

    Exercises for laboratory 1

--- Laborator 1/Exercitiul7.java
+++ Laborator 1/Exercitiul7.java
@@ -6,24 +6,29 @@ public class Exercitiul7 {
 
         System.out.print("Input n: ");
         int n = scanner.nextInt();
+        boolean primeExists = false;
 
         int factor = n;
         while (factor >= 2) {
-            boolean isPrime = true;
-            for (int d = 2; d <= n / 2; d++)
-                if (factor % d == 0) {
-                    isPrime = false;
+            if (n % factor == 0) {
+                boolean isPrime = true;
+                for (int d = 2; d <= factor / 2; d++)
+                    if (factor % d == 0) {
+                        isPrime = false;
+                        break;
+                    }
+
+                if (isPrime) {
+                    System.out.println("Biggest prime factor to divide " + n + ": " + factor);
+                    primeExists = true;
                     break;
                 }
-
-            if (isPrime) {
-                System.out.println("Biggest prime factor to divide " + n + ": " + factor);
-                break;
             }
 
             factor--;
         }
 
-        System.out.println("There is no prime factor to divide " + n + ".");
+        if (primeExists == false)
+            System.out.println("There is no prime factor to divide " + n + ".");
     }
 }
